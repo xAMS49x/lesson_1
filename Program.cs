c6110d8bc4bb7ac67e51bca5baa842303bcfdc96
xAMS49x <ams49game@gmail.com>
2026-04-03 23:45:50 +0300

    first commit for advanced

--- Program.cs
+++ Program.cs
@@ -3,109 +3,44 @@
     
     static void Main()
     {
-        // Console.ForegroundColor = ConsoleColor.Black;
-        // Console.BackgroundColor = ConsoleColor.DarkRed;
-        Console.WriteLine("Welcome! Pick the task you want to see (1-5): ");
-        int TaskNumber = Convert.ToInt32(Console.ReadLine());
+        Console.WriteLine("Вітаю у AB Fitness Tracker! Введіть свою ціль (у кроках): ");
+        float goal = float.Parse(Console.ReadLine());
         
-        switch(TaskNumber)
+        Console.WriteLine("Супер, тепер ввдеіть Ваш поточний результат: ");
+        float current = float.Parse(Console.ReadLine());
+        
+        float percentage = (current / goal) * 100;
+        if (percentage < 0)
         {
-            
-        case 1: 
-        // Task 1
-        Console.WriteLine("Enter the number a: ");
-        int a = Convert.ToInt32(Console.ReadLine());
-        
-        Console.WriteLine("Enter the number b: ");
-        int b = Convert.ToInt32(Console.ReadLine());
-        
-        Console.WriteLine($"Your result is:  {a * Math.Pow(b,2)}");
-
-            break;
-        
-        
-        // Task 2
-        case 2:
-        Console.WriteLine("Count yo days");
-        int days = Convert.ToInt32(Console.ReadLine());
-        int weeks = 0;
-        
-        while (days >= 7)
-        {
-            days -= 7;
-            weeks++;
+            Console.WriteLine("Значення не може бути від'ємним");
+            return;
         }
-        Console.WriteLine($"Yo days: weeks: {weeks}, days: {days}");
-
-            break;
         
+        Math.Round(percentage);
+
+        if (percentage < 70)
+        {
+            Console.WriteLine("Ну це не діло, треба більше рухатись!");
+        }
         
-        // Task 3
-        case 3:
-            Console.WriteLine("12% of number calculator. Enter your number: ");
-            double number = Convert.ToInt32(Console.ReadLine());
-            
-            Console.WriteLine($"12% of {number} is: {(number * 12) / 100}");
-
-            break;
+        else if (percentage > 70 && percentage < 90)
+        {
+            Console.WriteLine("Чудово, але треба активніше!");
+        }
         
-        // Task 4
-        case 4:
-            Console.WriteLine("Enter kilometer value: ");
-            double kilometer = double.Parse(Console.ReadLine());
-            
-            Console.WriteLine("Enter kilogram value: ");
-            double kgs = double.Parse(Console.ReadLine());
-            
-            Console.WriteLine("Enter liter value: ");
-            double liter = double.Parse(Console.ReadLine());
-            
-            Console.WriteLine($"\n{kilometer} kilometers is: {kilometer * 0.6213712} miles");
-            Console.WriteLine($"{kgs} kgs is: {kgs * 2.2046226} lbs");
-            Console.WriteLine($"{liter} liters is: {liter * 0.2641721} gallons");
-            
-            break;
-        // Task 5
-        case 5:
-            
-            Console.WriteLine("What is your name?");
-            string name = Console.ReadLine();
-            
-            Console.Write("Enter your year of birth: ");
-            int date = int.Parse(Console.ReadLine());
-            
-            int age = DateTime.Now.Year - date;
-            Console.WriteLine($"Welcome, {name}! You are ~{age} years old.");
-            
-            Console.WriteLine("\nIs that right? (y/n)");
-            string choice = Console.ReadLine();
+        else if (percentage > 90 && percentage < 99)
+        {
+            Console.WriteLine("Дуже близько, продовжуйте!");
+        }
+        
+        else if (percentage > 100 && percentage < 199)
+        {
+            Console.WriteLine("Ваша ціль досягнута!");
+        }
 
-            switch (choice) 
-            {
-                case "y":
-                    if (age <= 15)
-                    {
-                        Console.Write("yooo,go back to kindergarten");
-                    }
-
-                    if (age > 16)
-                    {
-                        Console.Write("I am comfortable talking to you then");
-                    }
-
-                    if (age > 30)
-                    {
-                        Console.Write("Oh my god, what are you, a fossil? Holy hell, dinosaur, bro");
-                    }
-                    
-                    break;
-                
-                case "n":
-                    Environment.Exit(0);
-                    break;
-            }
-            
-            break;
+        else if (percentage > 200)
+        {
+            Console.WriteLine("Та господі, куди ти так гониш");
         }
     }
 }
